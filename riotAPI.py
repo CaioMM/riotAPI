57037b4ed265b8d1c7351b2caacc027602f60f6a
Caio Cardoso <cmmcardoso@gmail.com>
2022-08-08 11:53:20 -0300

    Separated APIs for easier updates

--- riotAPI.py
+++ riotAPI.py
@@ -2,79 +2,169 @@
 # coding: utf-8
 import requests
 
-class RiotAPI():
-    
-    def __init__(self,region,apiKey,version):
-    
+class MatchAPI():
+    """
+    Class for access riot Match API
+    parameters:
+    region = americas, asia or europe
+    version = api version
+    apiKey = dev api_key for access
+    """
+    def __init__(self, region='americas', version='v5', apiKey=''):
         self.region = region
-        self.apiKey = apiKey
         self.version = version
-        self.url = "https://"+self.region+".api.riotgames.com/lol/"
-        
-    def requestSummonerInfoByName(self, summonerName):
-#         Retorna as informações do Invocador
-        self.summonerName = summonerName
-        self.response = requests.get(self.url+"summoner/"+self.version+"/summoners/by-name/"+self.summonerName+"?api_key="+self.apiKey)
-#        self.summonerId = self.summonerInfo.json()['id']
-#        self.accountId = self.summonerInfo.json()['accountId']
+        self.apiKey = apiKey
+        self.headers = {"X-Riot-Token": self.apiKey}
+    
+    def setUrl(self):
+        """
+        Set baser URL to api access
+        """
+        return f'https://{self.region}.api.riotgames.com/lol/match/{self.version}/matches/'
+
+    def playerHistory(self,puuid,queue,start,count):
+        """
+        Request for player match history
+        """
+        self.response = requests.get(self.setUrl()+'by-puuid/'+puuid+'/ids?queue='+str(queue)+'&start='+str(start)+'&count='+str(count),headers=self.headers)
         return self.response.json()
 
-    def requestSummonerInfoBySummonerId(self, summonerId):
-#         Retorna as informações do Invocador
-        self.summonerId = summonerId
-        self.response = requests.get(self.url+"summoner/"+self.version+"/summoners/"+self.summonerId+"?api_key="+self.apiKey)
-        return self.response.json()
-    
-    def requestCurrentGameInfo(self, summonerId):
-#         Retorna informações da partida em curso
-        self.summonerId = summonerId
-        self.response = requests.get(self.url+"spectator/"+self.version+"/active-games/by-summoner/"+self.summonerId+"?api_key="+self.apiKey)
-        return self.response.json()
-    
-    def requestMatchHistory(self, accountId, beginIndex=0,endIndex=0, queueId=0):
-#         Retorna o histórico de partida do invocador
-        self.accountId = accountId
-        self.beginIndex = beginIndex
-        self.endIndex = endIndex
-        self.queueId = queueId
-        
-#         Caso o Id da fila seja definido
-        if self.queueId != 0:
-               self.response = request.get(self.url+"match/"+self.version+"/matchlists/by-account/"+self.accountId+"?queue="+self.queueId+"?api_key="+self.apiKey)
-            
-#         Caso o número de partidas solicitados seja definido
-        if self.beginIndex == 0 and self.endIndex == 0:            
-               self.response = requests.get(self.url+"match/"+self.version+"/matchlists/by-account/"+self.accountId+"?api_key="+self.apiKey)
-        else:
-               self.response = requests.get(self.url+"match/"+self.version+"/matchlists/by-account/"+self.accountId+"?endIndex="+str(self.endIndex)+"&beginIndex="+str(self.beginIndex)+"&api_key="+self.apiKey)
-        return self.response.json()
-    
-    def requestSummonerRankedMatches(self, accountId):
-#         Retorna todas as partidas rankeadas de um Jogador
-        self.accountId = accountId
-        self.queueId = 420
-        self.response = requests.get(self.url+"match/"+self.version+"/matchlists/by-account/"+self.accountId+"?queue="+str(self.queueId)+"&api_key="+self.apiKey)
-        
-        return self.response.json()
-        
-    def requestMatchData(self, gameId):
-#         Retorna as informações de uma partida
-        self.gameId = gameId
-        self.response = requests.get(self.url+"match/"+self.version+"/matches/"+str(self.gameId)+"?api_key="+self.apiKey)
-        return self.response.json()
-    
-    def requestMatchTimeLine(self, gameId):
-#         Retorna os eventos de uma partida
-        self.gameId = gameId
-        self.response = requests.get(self.url+"match/"+self.version+"/timelines/by-match/"+str(self.gameId)+"?api_key="+self.apiKey)
+    def matchInfo(self,matchId):
+        """
+        Request specific match information
+        """
+        self.response = requests.get(self.setUrl()+matchId,headers=self.headers)
         return self.response.json()
 
-    def requestChallengerRank(self):
-#         Retorna os jogadores na divisão Desafiante na ordem em que estão rankeados
-        self.response = requests.get(self.url+"league-exp/"+self.version+"/entries/RANKED_SOLO_5x5/CHALLENGER/I?page=1&api_key="+self.apiKey)
-        return self.response.json()        
-
-    def requestPlayerChampionsMastery(self, summonerId):
-        self.summonerId = summonerId
-        self.response = requests.get(self.url+"champion-mastery/"+self.version+"/champion-masteries/by-summoner/"+self.summonerId+"?api_key="+self.apiKey)
+    def timeLine(self,matchId):
+        """
+        Request timeline of specific match
+        """
+        self.response = requests.get(self.setUrl()+matchId+'/timeline',headers=self.headers)
         return self.response.json()
+
+class LeagueAPI():
+    """
+    Class for access riot Match API
+    parameters:
+    region = br1, eun1, euw1, jp1, kr, la1, la2, na1, oc1, ru, tr1
+    version = api version
+    apiKey = dev api_key for access
+    """
+    def __init__(self, region, version, apiKey):
+        self.region = region
+        self.version = version
+        self.apiKey = apiKey
+        self.headers = {"X-Riot-Token": self.apiKey}
+
+    def setUrl(self):
+        """
+        Set baser URL to api access
+        """
+        return f'https://{self.region}.api.riotgames.com/lol/league/{self.version}/'
+
+    def challengerLeagues(self, queue):
+        """
+        return challengers players
+        queue: RANKED_SOLO_5x5, RANKED_FLEX_SR, RANKED_FLEX_TT
+        """
+        self.response = requests.get(self.setUrl()+'challengerleagues/by-queue/'+queue,headers=self.headers)
+        return self.response.json()
+
+    def entriesBySummoner(self,encryptedSummonerId):
+        """
+        return information of specific summoner
+        encryptedSummonerId: id of summoner (string)
+        """
+        self.response = requests.get(self.setUrl()+'entries/by-summoner/'+encryptedSummonerId,headers=self.headers)
+        return self.response.json()
+
+    def entries(self,queue,tier,division):
+        """
+        return information of summoners in an specific tier division queue
+        Get all the league entries
+        queue: RANKED_SOLO_5x5, RANKED_FLEX_SR, RANKED_FLEX_TT
+        tier: DIAMOND, PLATINUM, GOLD, SILVER, BRONZE, IRON
+        division: I, II, III, IV
+        """
+        self.response = requests.get(self.setUrl()+'entries/'+queue+'/'+tier+'/'+division+'?page=1',headers=self.headers)
+        return self.response.json()
+
+    def grandMasterLeagues(self, queue):
+        """
+        return grandmaster players
+        queue: RANKED_SOLO_5x5, RANKED_FLEX_SR, RANKED_FLEX_TT
+        """
+        self.response = requests.get(self.setUrl()+'grandmasterleagues/by-queue/'+queue,headers=self.headers)
+        return self.response.json()
+
+    def leagues(self, leagueId):
+        """
+        return league info by league id
+        leagueId: id of league (string)
+        """
+        self.response = requests.get(self.setUrl()+'leagues/'+leagueId,headers=self.headers)
+        return self.response.json()
+
+    def masterLeagues(self, queue):
+        """
+        return master players
+        queue: RANKED_SOLO_5x5, RANKED_FLEX_SR, RANKED_FLEX_TT
+        """
+        self.response = requests.get(self.setUrl()+'masterleagues/by-queue/'+queue,headers=self.headers)
+        return self.response.json()
+
+class SummonerAPI():
+    """
+    Class for access riot Summoner API
+    parameters:
+    region = br1, eun1, euw1, jp1, kr, la1, la2, na1, oc1, ru, tr1
+    version = api version
+    apiKey = dev api_key for access
+    """
+    def __init__(self, region, version, apiKey):
+        self.region = region
+        self.version = version
+        self.apiKey = apiKey
+        self.headers = {"X-Riot-Token": self.apiKey}
+
+    #/lol/summoner/v4/summoners/by-account/{encryptedAccountId}
+    def setUrl(self):
+        """
+        Set baser URL to api access
+        """
+        return f'https://{self.region}.api.riotgames.com/lol/summoner/{self.version}/'
+
+    def byAccount(self,encryptedAccountId):
+        """
+        return summoner info by encrypted account id
+        encryptedAccountId: encrypted account id (string)
+        """
+        self.response = requests.get(self.setUrl()+'summoners/by-account/'+encryptedAccountId,headers=self.headers)
+        return self.response.json()
+
+    def byName(self,summonerName):
+        """
+        return summoner info by summoner name
+        summonerName: summoner name (string)
+        """
+        self.response = requests.get(self.setUrl()+'summoners/by-name/'+summonerName,headers=self.headers)
+        return self.response.json()
+
+    def byPUUID(self,encryptedPUUID):
+        """
+        return summoner info by encryptedPUUID
+        encryptedPUUID: encrypted PUUID (string)
+        """
+        self.response = requests.get(self.setUrl()+'summoners/by-puuid/'+encryptedPUUID,headers=self.headers)
+        return self.response.json()
+
+    def bySummonerID(self,encryptedSummonerId):
+        """
+        return summoner info by encrypted Summoner Id
+        encryptedSummonerId: encrypted Summoner Id (string)
+        """
+        self.response = requests.get(self.setUrl()+'summoners/'+encryptedSummonerId,headers=self.headers)
+        return self.response.json()
+
+    
